1d570d92d381b81c82009aaf03f93e18d14f2619
Chan Thai Thong <thongz0819@live.com>
2025-09-05 10:25:35 +0800

    added in QR image generation

--- src/SessionTab.js
+++ src/SessionTab.js
@@ -21,6 +21,7 @@ export default function SessionTab({ sessionId, setSessionId, chatData, setChatD
       <div className="row"><button className="primary" onClick={handleClick}>Generate</button></div>
       <div className="row"><span className="label">Session ID:</span> <span>{sessionId || "N/A"}</span></div>
       <div className="row"><span className="label">Chat Data:</span> <span>{chatData || "N/A"}</span></div>
+      {sessionId ? <div className="row"><div><img src={`https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=${sessionId}`}></img></div></div> : null}
     </div>
   );
 }
